98d90c780f5652242a496554fc2bba9ef67ec6d1
LeNa24Shi <lena.shihab@gritacademy.se>
2025-06-16 20:45:02 +0200

    Add log in page and sign up page with styling

---
+++ src/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="formContainer">
+
+        <h2>Log In</h2>
+        <form action="">
+            <input type="text" name="username" placeholder="User Name">
+            <input type="password" name="userpassword" placeholder="Password">
+            <button>Log In</button>
+        </form>
+        <p>Don't have an account yet ? <a href="#">Sign Up</a></p>
+
+    </div>
+
+</body>
+
+</html>
---
+++ src/signup.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <div class="formContainer">
+
+        <h2>Log In</h2>
+        <form action="">
+            <input type="text" name="username" placeholder="User Name">
+            <input type="password" name="userpassword" placeholder="Password">
+            <input type="e-post" name="usere-post" placeholder="E-post">
+            <button>Sign Up</button>
+        </form>
+        <p>Already have an account ? <a href="#">Log In</a></p>
+
+    </div>
+
+</body>
+
+</html>
